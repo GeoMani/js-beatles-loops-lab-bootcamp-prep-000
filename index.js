204970649a2d881f0eb38574e74ba52679eacd7e
George Samuel Igoe <george.igoe@gmail.com>
2018-06-13 03:16:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function theBeatlesPlay(beatles, instrus) {
+  
+  var whoPlaysWhat = [];
+  
+  for (var i = 0; i < beatles.length; i++) {
+    whoPlaysWhat.push(`${beatles[i]} plays ${instrus[i]}`);
+  }
+  
+  return whoPlaysWhat;
+
+}
+
+
+
+function johnLennonFacts(facts) {
+  var j = 0;
+  
+  while(j <= facts.length) {
+    console.log(`${facts[j]}`)
+    j = j++;
+  }
+}
