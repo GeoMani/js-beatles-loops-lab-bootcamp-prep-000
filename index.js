0a447ccc22c87b3fca9248372f688ac10ed00e08
George Samuel Igoe <george.igoe@gmail.com>
2018-06-13 03:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function theBeatlesPlay(beatles, instrus) {
 function johnLennonFacts(facts) {
   var j = 0;
   var array = [];
-  while(j <= facts.length) {
+  while(j < facts.length) {
     
     array.push(`${facts[j]}!!!`)
     j++
